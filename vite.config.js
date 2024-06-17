bb7f604289b8ae8f2fe3265f74e4d082b30724ba
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-06-17 15:30:00 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Text-Converter/',
   plugins: [react()],
-})
+  base: '/Text-Converter/', // base URL for your project
+});
